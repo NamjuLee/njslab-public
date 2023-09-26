cb2bfbc3a97747dad16abeb0b2a538f0fb083eb7
NJ <nj.namju@gmail.com>
2023-09-26 19:58:15 +0900

    update

--- njs-lab-legacy/7086.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[7086],{7086:(n,o,t)=>{t.r(o),t.d(o,{AppSmartDrawing:()=>i});var i=function(){function n(n){void 0===n&&(n="SandBox"),console.log("init smart drawing...")}return n.Init=function(o){void 0===o&&(o="SandBox"),new n(o).Dummy()},n.prototype.Dummy=function(){},n}()}}]);
